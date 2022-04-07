df2083711f15c3ecd52bcc0580c5845534f95833
namutehani <64838278+namutehani2@users.noreply.github.com>
2022-04-07 19:06:11 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -60,7 +60,6 @@ async def send_msg(user_id, message):
     except Exception as e:
         return 500, f"{user_id} : {traceback.format_exc()}\n"
 
-
 async def broadcast_handler(m: Message):
     all_users = await db.get_all_users()
     broadcast_msg = m.reply_to_message
@@ -76,34 +75,37 @@ async def broadcast_handler(m: Message):
     done = 0
     failed = 0
     success = 0
+    broadcast_ids[broadcast_id] = dict(
+        total=total_users,
+        current=done,
+        failed=failed,
+        success=success
+    )
     async with aiofiles.open('broadcast.txt', 'w') as broadcast_log_file:
         async for user in all_users:
-            try:
-                sts, msg = await send_msg(
+            sts, msg = await send_msg(
                 user_id=int(user['id']),
                 message=broadcast_msg
             )
-                if msg is not None:
-                    await broadcast_log_file.write(msg)
-                if sts == 200:
-                    success += 1
-                else:
-                    failed += 1
-                if sts == 400:
-                    await db.delete_user(user['id'])
-                    done += 1
-                if broadcast_ids.get(broadcast_id) is None:
-                    break
-                else:
-                    broadcast_ids[broadcast_id].update(
+            if msg is not None:
+                await broadcast_log_file.write(msg)
+            if sts == 200:
+                success += 1
+            else:
+                failed += 1
+            if sts == 400:
+                await db.delete_user(user['id'])
+            done += 1
+            if broadcast_ids.get(broadcast_id) is None:
+                break
+            else:
+                broadcast_ids[broadcast_id].update(
                     dict(
                         current=done,
                         failed=failed,
                         success=success
                     )
                 )
-            except:
-                pass
     if broadcast_ids.get(broadcast_id):
         broadcast_ids.pop(broadcast_id)
     completed_in = datetime.timedelta(seconds=int(time.time() - start_time))
@@ -124,7 +126,8 @@ async def broadcast_handler(m: Message):
                     f"Total done {done}, {success} success and {failed} failed.",
             quote=True
         )
-    await aiofiles.os.remove('broadcast.txt')        
+    await aiofiles.os.remove('broadcast.txt')
+
 #=====================================================================================##
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
