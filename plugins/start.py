1ae55eda6dff16cb31f05853afe29b862e347c44
namutehani <64838278+namutehani2@users.noreply.github.com>
2022-04-15 21:48:40 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -190,11 +190,11 @@ async def start_command(client: Client, message: Message):
             else:
                 reply_markup = None
             try:
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup,protect_content=True)
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
                 await asyncio.sleep(0.5)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup,protect_content=True)
+                await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = 'html', reply_markup = reply_markup)
             except:
                 pass
         return
